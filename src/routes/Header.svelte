1cb34223cf24b5018b6502f6055ab63a5deb7a48
Fabian Wünderich <fabian@wuenderich.de>
2023-08-30 03:20:47 +0200

    Fix build

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,7 +1,5 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
 </script>
 
 <header>
@@ -40,20 +38,6 @@
 	.corner {
 		width: 3em;
 		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
 	}
 
 	nav {
