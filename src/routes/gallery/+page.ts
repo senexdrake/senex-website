2920c820d1441a4450c451adbd1407af6d836fdc
Fabian Wünderich <fabian@wuenderich.de>
2023-09-02 05:27:05 +0200

    Update gallery meta description

--- src/routes/gallery/+page.ts
+++ src/routes/gallery/+page.ts
@@ -3,6 +3,7 @@ import type {Metadata} from "$model/types";
 export function load() {
     return <Metadata>{
         title: "Senex's Gallery",
-        description: "Coming Soon!"
+        description: "This little site contains a select collection of artworks (specially Refsheets) " +
+            "I've commissioned over the past years for my fursona."
     }
 }
