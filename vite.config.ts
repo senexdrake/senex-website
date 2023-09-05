8815ec64dcc2866a1b7ab00e8688fedc25bb06b9
Fabian Wünderich <fabian@wuenderich.de>
2023-09-05 13:51:37 +0200

    Remove unneeded code

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { imagetools } from "vite-imagetools";
-import { assetDownloader } from "./src/lib/vite/vite-asset-downloader";
 import {staticImageHandler} from "./src/lib/vite/vite-static-image-handler";
 
 export default defineConfig({
