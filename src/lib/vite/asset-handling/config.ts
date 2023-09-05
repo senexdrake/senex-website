098ddaf767c5179aff5f75e66df5c7b5eb679588
Fabian Wünderich <fabian@wuenderich.de>
2023-09-05 14:13:47 +0200

    Increase image transformation quality

--- src/lib/vite/asset-handling/config.ts
+++ src/lib/vite/asset-handling/config.ts
@@ -6,9 +6,9 @@ export const fileEncoding = 'utf-8'
 export const remoteAssetBaseUrl = (process.env.REMOTE_ASSETS_BASE_URL ?? "https://pics.arisendrake.de") + '/'
 
 export const processingRules: ProcessingRule[] = [
-    { maxDimension: 600, quality: 50, format: defaultImageType },
-    { maxDimension: 1200, quality: 50, format: defaultImageType },
-    { maxDimension: 2400, quality: 50, format: defaultImageType },
+    { maxDimension: 600, quality: 65, format: defaultImageType },
+    { maxDimension: 1200, quality: 65, format: defaultImageType },
+    { maxDimension: 2400, quality: 65, format: defaultImageType },
     { maxDimension: 4000, quality: 90, format: defaultImageType }
 ]
 export const originalTransformQuality = 95
