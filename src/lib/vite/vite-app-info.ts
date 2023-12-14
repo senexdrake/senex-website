0b36c1c469cc6812e6389384852b2830adcc0fee
Fabian Wünderich <fabian@wuenderich.de>
2023-12-14 23:22:56 +0100

    Add ability to determine version by calling git

--- src/lib/vite/vite-app-info.ts
+++ src/lib/vite/vite-app-info.ts
@@ -2,6 +2,12 @@ import type {Plugin} from "vite";
 import {writeFile} from "fs/promises";
 import {dataDir, appInfoFile} from "../../config"
 import {ensurePathExists} from "../util";
+import {exec as origExec} from "child_process"
+import {promisify} from "util";
+
+const exec = promisify(origExec)
+
+const tryGitExecutableForVersion = true
 
 export interface AppInfo {
     version: string
@@ -12,7 +18,15 @@ export function appInfo() : Plugin {
         enforce: 'post',
         name: 'app-info',
         async buildStart() {
-            const version = process.env.VERSION_HASH ?? process.env.CF_PAGES_COMMIT_SHA ?? "master"
+            let version = process.env.VERSION_HASH ?? process.env.CF_PAGES_COMMIT_SHA
+            if (!version && tryGitExecutableForVersion) {
+                const gitCommand = "git rev-parse HEAD"
+                await exec(gitCommand).then(res => {
+                    version = res.stdout.trim()
+                })
+            }
+
+            if (!version) version = "unknown"
 
             await ensurePathExists(dataDir)
             await writeFile(appInfoFile, JSON.stringify(<AppInfo>{
