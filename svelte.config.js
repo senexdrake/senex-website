235f36636cda84b18eeacb892b4fd27cb4a9520e
Fabian Wünderich <fabian@wuenderich.de>
2023-12-17 04:29:06 +0100

    Upgrade to SevelteKit v2

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import {dataDir, remoteAssetsRelative} from "./src/config.js";
 
 const dev = true
