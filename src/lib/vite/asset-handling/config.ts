3887242675773b169d43270e6b08f775bff6f5c4
Fabian Wünderich <fabian@wuenderich.de>
2024-02-12 17:01:15 +0100

    Increase chunk size to 20

--- src/lib/vite/asset-handling/config.ts
+++ src/lib/vite/asset-handling/config.ts
@@ -1,3 +1,5 @@
+import {addTrailingSlash} from "./util";
+
 export const debug = false
 export const defaultImageType = 'webp'
 export const fileEncoding = 'utf-8'
@@ -5,7 +7,7 @@ export const fileEncoding = 'utf-8'
 export const assetsServer = (process.env.ASSETS_SERVER ?? "https://pics.zdrake.net")
 export const assetsServerPath = (process.env.ASSETS_SERVER_PATH ?? addTrailingSlash(assetsServer) + "export") + '/'
 
-export const imageFetchChunkSize = 10
+export const imageFetchChunkSize = 20
 
 export const metaMaxHeight = 1000
 export const defaultCategory = 'images'
