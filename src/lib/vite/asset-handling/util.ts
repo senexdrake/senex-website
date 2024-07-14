ddacc84e60d22ec63bd47ef7fffb3add0b4f93f9
Fabian Wünderich <fabian@wuenderich.de>
2024-07-14 04:10:44 +0200

    Update and cleanup dependencies, remove marked

--- src/lib/vite/asset-handling/util.ts
+++ src/lib/vite/asset-handling/util.ts
@@ -2,7 +2,6 @@ import crypto from "crypto";
 import type {ImageAuthor} from "../../model/types";
 import {defaultImageType} from "./config";
 import type {ImageRaw} from "./types";
-import {marked} from "marked";
 import {chalk} from "../../util";
 
 export {clearPath, addTrailingSlash, chalk, formatBytes, pathExists} from "../../util";
@@ -41,63 +40,6 @@ export function formattedDuration(start: number, end: number = Date.now()): stri
 export function formatTime(time: number): string {
     return (time / 1000).toString() + "s"
 }
-
-function createPlainTextRenderer() {
-    const htmlEscapeToText = (text) => {
-        return text.replace(/\&\#[0-9]*;|&amp;/g, function (escapeCode) {
-            if (escapeCode.match(/amp/)) {
-                return '&';
-            }
-            return String.fromCharCode(escapeCode.match(/[0-9]+/));
-        });
-    }
-
-    const render = new marked.Renderer();
-
-    render.text = (text) => {
-        return text
-    }
-
-    render.br = () => {
-        return "\n";
-    }
-
-    render.del = (text) => {
-        return text
-    }
-
-    render.codespan = (code) => {
-        return code
-    }
-
-    render.em = (text) => {
-        return text
-    }
-
-    // render just the text of a link
-    render.link = (href, title, text) => {
-        return text;
-    };
-
-    // render just the text of a paragraph
-    render.paragraph = function (text) {
-        return htmlEscapeToText(text)+'\r\n';
-    };
-
-    // render just the text of a heading element, but indecate level
-    render.heading = (text, level) => {
-        return level + ' ) ' + text;
-    };
-
-    // render nothing for images
-    render.image = () => {
-        return '';
-    };
-
-    return render;
-}
-
-export const plainTextRenderer = createPlainTextRenderer()
 
 export const timeLog = chalk.green
 export const fileSizeLog = chalk.yellow
