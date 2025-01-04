dff110bfadff9dac14f41699daada9aacc6c2c39
Fabian Wünderich <fabian@wuenderich.de>
2025-01-05 00:00:03 +0100

    Cache resolved links

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,32 +4,37 @@ import {allLinks} from "$model/links";
 import {RESOLVE_LINKS} from "$env/static/private";
 
 const resolveParallel = true
+const resolveLinks = async (links: LinkItem[]): Promise<Map<string, string>> => {
+    const linkMap = new Map<string, string>()
+    if (!toBoolean(RESOLVE_LINKS)) {
+        console.log("Not resolving links")
+        return linkMap
+    }
+    console.log("Resolving links...")
+    console.log("Parallel:", resolveParallel)
+    const promises: Promise<string>[] = []
+    for (const link of links) {
+        if (!link.target.startsWith("http")) continue
+
+        const resolved = resolveLink(link.target).then(resolved =>
+            linkMap.set(link.target, resolved)
+        )
+        promises.push(resolved)
+        if (!resolveParallel) await resolved
+    }
+
+    await Promise.all(promises)
+    return linkMap
+}
+
+let linkMap: Map<string, string>|undefined = undefined
 
 export async function load() {
-    const resolveLinks = async (links: LinkItem[]): Promise<Map<string, string>> => {
-        const linkMap = new Map<string, string>()
-        if (!toBoolean(RESOLVE_LINKS)) {
-            console.log("Not resolving links")
-            return linkMap
-        }
-        console.log("Resolving links...")
-        console.log("Parallel:", resolveParallel)
-        const promises: Promise<string>[] = []
-        for (const link of links) {
-            if (!link.target.startsWith("http")) continue
-
-            const resolved = resolveLink(link.target).then(resolved =>
-                linkMap.set(link.target, resolved)
-            )
-            promises.push(resolved)
-            if (!resolveParallel) await resolved
-        }
-
-        await Promise.all(promises)
-        return linkMap
+    if (!linkMap) {
+        linkMap = await resolveLinks(allLinks)
     }
 
     return <Metadata|LinkData>{
-        linkMap: await resolveLinks(allLinks)
+        linkMap: linkMap
     }
 }
