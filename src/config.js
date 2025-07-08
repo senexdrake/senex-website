ece8c4a7a5b5e8372d55dde78dc09db3ff0d265f
Fabian Wünderich <fabian@wuenderich.de>
2025-07-08 15:50:37 +0200

    Disable link to repository

--- src/config.js
+++ src/config.js
@@ -14,7 +14,7 @@ export const galleryAssetBaseUrl = '/' + staticAssetsPrefix
 export const defaultTitle = "ZenDrake, the big Bull-Dragon"
 export const defaultDescription = "An overview of ways to find, contact and find out more about the big Bull-Dragon!"
 
-export const linkToRepo = true
+export const linkToRepo = false
 export const repoUrl = "https://github.com/senexdrake/senex-website"
 export const versionEnvironmentName = "CF_PAGES_COMMIT_SHA"
 
