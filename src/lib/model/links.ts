d40e90e3046e3e5ea26bb4b2fafde56bb0470d04
Fabian Wünderich <fabian@wuenderich.de>
2026-01-05 04:07:08 +0100

    Remove order attribute from links. Insert order will determine sorting.

--- src/lib/model/links.ts
+++ src/lib/model/links.ts
@@ -21,28 +21,29 @@ const shortLinkBase = "https://zdrake.net"
 const addBaseUrl = (target: string) => stripTrailingSlash(shortLinkBase) + target
 
 export const bigLinks: LinkItem[] = [
-    { name: 'X (Twitter)', target: '/tw-fur', icon: IconX, order: 1 },
-    { name: 'Itaku', target: '/itaku', icon: IconItaku, order: 21 },
-    { name: 'FurAffinity', target: '/fa', icon: IconFuraffinity, order: 3 },
-    { name: 'Mastodon', target: '/mastodon', icon: IconMastodon, order: 20 },
-    { name: 'BlueSky', target: '/bsky-fur', icon: IconBluesky, order: 5 },
-    { name: 'Telegram', target: '/telegram', icon: IconTelegram, order: 8 },
-    { name: 'Character Refs', target: '/drake-refs', icon: IconArt, fullWidth: false, order: 1001 },
+    { name: 'FurAffinity', target: '/fa', icon: IconFuraffinity },
+    { name: 'BlueSky', target: '/bsky-fur', icon: IconBluesky },
+    { name: 'Telegram', target: '/telegram', icon: IconTelegram },
+    { name: 'X (Twitter)', target: '/tw-fur', icon: IconX },
+    { name: 'Itaku', target: '/itaku', icon: IconItaku },
+    { name: 'Mastodon', target: '/mastodon', icon: IconMastodon },
+
+    { name: 'Character Refs', target: '/drake-refs', icon: IconArt, fullWidth: false },
     //{ name: "Discord", target: "/discord", order: 10 },
-    { name: "BARQ!", target: "/barq", order: 11, icon: IconBarq }
+    { name: "BARQ!", target: "/barq", icon: IconBarq }
 ]
 bigLinks.forEach(link => link.linkType = LinkType.BUTTON)
 
 export const smallLinks: LinkItem[] = [
-    { name: 'Steam', target: '/steam', icon: IconSteam, order: 100 },
-    { name: 'E-Mail', target: `mailto:${contactEmail}`, icon: IconEmail, order: 110 },
+    { name: 'Steam', target: '/steam', icon: IconSteam },
+    { name: 'E-Mail', target: `mailto:${contactEmail}`, icon: IconEmail },
     //{ name: 'Furry Network', target: '/fn', order: 120 },
 ]
 smallLinks.forEach(link => link.linkType = LinkType.BUTTON_SMALL)
 
-export const discordLink: LinkItem = { name: 'Discord', target: '/discord', order: 10000, icon: IconDiscord, linkType: LinkType.SPECIAL }
+export const discordLink: LinkItem = { name: 'Discord', target: '/discord', icon: IconDiscord, linkType: LinkType.SPECIAL }
 
-export const allLinks: LinkItem[] = [...bigLinks, ...smallLinks, discordLink].sort((a, b) => a.order - b.order)
+export const allLinks: LinkItem[] = [...bigLinks, ...smallLinks, discordLink]
 export const allLinksCopy = () => allLinks.map(link => Object.assign({}, link))
 export const processLinkTarget = (link: LinkItem) => {
     if (link.target.startsWith("/"))
--- src/lib/model/types.d.ts
+++ src/lib/model/types.d.ts
@@ -4,7 +4,6 @@ import type {Component} from "svelte";
 export interface LinkItem {
     target: string,
     name: string,
-    order: number
     icon?: Component,
     linkType?: LinkType,
     fullWidth?: boolean
