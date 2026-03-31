d4016b21d9a4974a3fa5b205c91875b24818eb87
Fabian Wünderich <fabian@wuenderich.de>
2026-03-31 22:53:30 +0200

    Update curl version in user agent string

--- src/config.js
+++ src/config.js
@@ -25,5 +25,5 @@ export const contactEmail = "furry@zdrake.net"
 
 export const pwaThemeColor = "#4075a6"
 export const pwaBackgroundColor = "#424242"
-export const linkResolverUserAgent = "curl/7.88.1"
+export const linkResolverUserAgent = "curl/8.14.1"
 export const linkToProfileBanner = true
